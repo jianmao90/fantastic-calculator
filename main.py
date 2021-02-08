2742750b5709c6e696bd4e26b2f282b25b21a15a
Louis Haeberlé <louis.haeberle@gmail.com>
2021-02-08 16:13:18 -0500

    uh oh, conflict time

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ area=areaCalc.square(a)
 perimeter=perimCalc.square(a)
 print('For a square of length %d, the area is %d and the perimeter %d'
 %(a,area,perimeter))
+
+r=2
+area_circle = areaCalc.circle(r)
+perimeter_circle = perimCalc.circle(r)
+print('For a circle of radius %d, the area is %d and the circumference is %d'%(r, area_circle, perimeter_circle))
