1e9bddfd34dd5cfb42237dd4dafc15eee1a36aa2
Félix Thouin <felix.thouin@gmail.com>
2021-02-08 15:54:31 -0500

    Now computes volume!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import areaCalc 
 import perimCalc 
 import numpy as np
+import volume
 
 # Square demo
 a=5
@@ -8,3 +9,7 @@ area=areaCalc.square(a)
 perimeter=perimCalc.square(a)
 print('For a square of length %d, the area is %d and the perimeter %d'
 %(a,area,perimeter))
+# Cube demo
+a=5
+vol=volume.cube(a)
+print('The volume of a cube of size %d is %d'%(a,vol))
